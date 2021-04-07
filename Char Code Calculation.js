9a38721e626590a050429763dade47bb5fdc827a
Edgars Gaidels <egaidels@Edgarss-MacBook-Pro.local>
2021-04-07 19:06:52 +0300

    adding another file

---
+++ Char Code Calculation.js
@@ -0,0 +1,22 @@
+function calc(x){
+    let sum = []; 
+    let arr = x.split("");
+    
+    for (i = 0; i < arr.length; i++) {
+      sum.push(arr[i].charCodeAt());
+  }
+    
+    const total1 = sum.join('');
+    const total2 = total1.split("7").join("1");
+    
+    const total1Arr = total1.split("");
+    const total2Arr = total2.split("");
+    
+    let sumDifference = [];
+    
+    for (i = 0; i < total1Arr.length; i++) {
+      sumDifference.push(total1Arr[i] - total2Arr[i]);
+    }
+    
+    return sumDifference.reduce((a, b) => a + b);
+  }
