b5b0225ed5c4e79fa4fc4706cd190e723e112600
Edgars Gaidels <edgars.gaidels@gmail.com>
2021-04-08 17:13:30 +0300

    refactoring

--- IPv4 to int32.js
+++ IPv4 to int32.js
@@ -1,15 +1,8 @@
 //6 kyu - IPv4 to int32: https://www.codewars.com/kata/52ea928a1ef5cfec800003ee
 
 function ipToInt32(ip){
-    let octetsBinaryList = [];
-    
-    ip.split(".").forEach(element => {
-        let octetBinary = parseInt(element).toString(2);
-        while (octetBinary.length < 8){
-            octetBinary = "0" + octetBinary;
-        }
-        octetsBinaryList.push(octetBinary);
-    });
-    
-    return parseInt(octetsBinaryList.join(""), 2);
+    return parseInt(
+        ip.split(".")
+        .map(x => parseInt(x).toString(2).padStart(8, "0"))
+        .reduce((a, b) => (a + b)), 2);
 }
