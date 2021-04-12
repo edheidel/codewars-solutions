80783b3fb9ccf3d3a07c6d0a211f13d0d2d9f21f
Andris Vilde <vilde.andris@gmail.com>
2021-04-12 11:47:39 +0300

    Can be just in one line

--- IPv4 to int32.js
+++ IPv4 to int32.js
@@ -1,8 +1,5 @@
 //6 kyu - IPv4 to int32: https://www.codewars.com/kata/52ea928a1ef5cfec800003ee
 
-function ipToInt32(ip){
-    return parseInt(
-        ip.split(".")
-        .map(x => parseInt(x).toString(2).padStart(8, "0"))
-        .join(""), 2);
-}
+function ipToInt32(ip) {
+    return parseInt(ip.split(".").map(x => parseInt(x).toString(2).padStart(8, "0")).join(""), 2);
+} 
